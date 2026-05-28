dd5dcc1ac1cf54131132a2a5acd526e571f33ec0
João Barros <joaovictorpiresdev@gmail.com>
2026-05-28 19:50:55 -0300

    feat: Adding the student exercise

---
+++ src/Class1/Exercise5/Main.java
@@ -0,0 +1,11 @@
+package Class1.Exercise5;
+
+public class Main {
+    static void main() {
+            Student student1 = new Student();
+            student1.name = "Victor";
+            student1.age = 20;
+
+            student1.showSpecs();
+    }
+}
---
+++ src/Class1/Exercise5/Student.java
@@ -0,0 +1,11 @@
+package Class1.Exercise5;
+
+public class Student {
+    String name;
+    int age;
+
+    void showSpecs () {
+        System.out.println("Name : " + name);
+        System.out.println("Age  : " + age);
+    }
+}
