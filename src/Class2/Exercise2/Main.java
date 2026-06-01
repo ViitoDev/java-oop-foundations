afa617c6a3b97ae42cab3208c5b7cb9abba7dd0c
João Barros <joaovictorpiresdev@gmail.com>
2026-06-01 20:24:56 -0300

    feat: adding the exercise 2

---
+++ src/Class2/Exercise2/Main.java
@@ -0,0 +1,14 @@
+package Class2.Exercise2;
+
+public class Main {
+    static void main() {
+        People people = new People();
+
+        people.setAge(17);
+        people.setName("Victor");
+
+        System.out.println("Name : " + people.getName());
+        System.out.println("Age  : " + people.getAge());
+        people.checkAge(people.getAge());
+    }
+}
---
+++ src/Class2/Exercise2/People.java
@@ -0,0 +1,30 @@
+package Class2.Exercise2;
+
+public class People {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void checkAge(int age){
+        if (age >= 18) {
+            System.out.println("You are 18 or older");
+        } else {
+            System.out.println("You are over 18 years old");
+        }
+    }
+}
