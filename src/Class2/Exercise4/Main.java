f8ad8ed8139ffeced512bc90bee48fa168aaabc7
João Barros <joaovictorpiresdev@gmail.com>
2026-06-02 07:07:13 -0300

    feat: adding the exercise 4

---
+++ src/Class2/Exercise4/Main.java
@@ -0,0 +1,13 @@
+package Class2.Exercise4;
+
+public class Main {
+    static void main() {
+        Student student = new Student("Victor",0,5,10);
+
+        System.out.println("Student name  : " + student.getName());
+        System.out.println("Note 1        : " + student.getNote1());
+        System.out.println("Note 2        : " + student.getNote2());
+        System.out.println("Note 3        : " + student.getNote3());
+        System.out.println("Student media : " + student.calculateMedia());
+    }
+}
