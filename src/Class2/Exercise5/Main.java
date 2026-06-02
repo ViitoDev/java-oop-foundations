12c30a8824bf0b6776f457e627d69a2767ec1dba
João Barros <joaovictorpiresdev@gmail.com>
2026-06-02 07:54:00 -0300

    feat: addingthe exercise 5

---
+++ src/Class2/Exercise5/Main.java
@@ -0,0 +1,11 @@
+package Class2.Exercise5;
+
+public class Main {
+    static void main() {
+        Book book1 = new Book();
+        book1.setAuthor("Machiavelli");
+        book1.setTitle("The Prince");
+
+        book1.bookSpecs();
+    }
+}
